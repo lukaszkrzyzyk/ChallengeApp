8bec8c83cf07c9ef330f23998f52346a6af96e1d
Lukasz Krzyzyk <krzyzyklukasz@gmail.com>
2023-03-07 15:04:55 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// zadanie dzień 4
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Ewa";
+        string gender = "kobieta";
+        int age = 30;
+
+        if (gender == "kobieta" && age < 30)
+        {
+            Console.WriteLine("Kobieta poniżej 30 lat");
+        }
+        else if (name == "Ewa" && age == 30)
+        {
+            Console.WriteLine("Ewa, lat 30");
+        }
+        else if (gender == "mężczyzna" && age < 18)
+        {
+            Console.WriteLine("Niepełnoletni mężczyzna");
+        }
+        else
+        {
+            Console.WriteLine("Nie spełniono żadnego warunku.");
+        }
+    }
+}
