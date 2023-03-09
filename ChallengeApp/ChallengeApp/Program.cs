bf4b19adbefb98d86e2eaaba3c8bbdad473c8683
Lukasz Krzyzyk <krzyzyklukasz@gmail.com>
2023-03-09 14:59:34 +0100

    Zadanie 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,20 @@
-﻿using System;
+﻿
 
 class Program
 {
     static void Main(string[] args)
     {
-        int number = 45660028;
+        User user1 = new User("Andrzej", "Dudek", 35, new List<int>() { 5, 8, 6, 9, 7 });
+        User user2 = new User("Katarzyna", "Madej", 28, new List<int>() { 6, 7, 5, 8, 9 });
+        User user3 = new User("Michał", "Maniara", 55, new List<int>() { 9, 8, 10, 7, 6 });
 
-        int[] counts = new int[10]; 
+        List<User> users = new List<User>() { user1, user2, user3 };
 
-        foreach (char digitChar in number.ToString()) 
-        {
-            int digit = int.Parse(digitChar.ToString()); 
+        User bestUser = users.OrderByDescending(u => u.GetTotalPoints()).First();
 
-            counts[digit]++; 
-        }
-
-        
-        for (int i = 0; i < counts.Length; i++)
-        {
-            Console.WriteLine("Cyfra {0}: {1} wystąpień", i, counts[i]);
-        }
+        Console.WriteLine($"Najlepszy użytkownik: {bestUser.FirstName} {bestUser.LastName}");
+        Console.WriteLine($"Wiek: {bestUser.Age}");
+        Console.WriteLine($"Punkty: {string.Join(", ", bestUser.Points)}");
+        Console.WriteLine($"Suma punktów: {bestUser.GetTotalPoints()}");
     }
 }
-
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public List<int> Points { get; set; }
+
+    public User(string firstName, string lastName, int age, List<int> points)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Points = points;
+    }
+
+    public int GetTotalPoints()
+    {
+        return Points.Sum();
+    }
+}
